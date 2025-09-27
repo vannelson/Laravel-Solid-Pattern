2eaf109ab67734d4037406357477791f1ccac869
vannelson <vannelsonsimumbay@gmail.com>
2025-09-27 13:59:58 +0800

    fic build issue

--- routes/modules/users.php
+++ routes/modules/users.php
@@ -1,12 +1,46 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
-// Public: register a user (apply tighter auth throttle)
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AlbumController;
+use App\Http\Controllers\SongController;
+use App\Http\Controllers\ReactionController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\CarRateController;
+use App\Http\Controllers\BookingController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
+
+// Default Laravel user check
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+/*
+|--------------------------------------------------------------------------
+| Authentication
+|--------------------------------------------------------------------------
+*/
+Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:auth');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+/*
+|--------------------------------------------------------------------------
+| Users
+|--------------------------------------------------------------------------
+*/
+// Public: register a user
 Route::post('users', [UserController::class, 'store'])->middleware('throttle:auth');
 
-// Protected: all other user routes require token
+// Protected
 Route::middleware('auth:sanctum')->group(function () {
     // Admin-only endpoints
     Route::middleware('admin')->group(function () {
@@ -18,4 +52,75 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('users/{id}', [UserController::class, 'show']);
     Route::put('users/{id}', [UserController::class, 'update']);
     Route::patch('users/{id}', [UserController::class, 'update']);
+});
+
+/*
+|--------------------------------------------------------------------------
+| Albums
+|--------------------------------------------------------------------------
+*/
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/albums', [AlbumController::class, 'list'])->name('albums.list');
+    Route::post('/albums', [AlbumController::class, 'create'])->name('albums.create');
+    Route::put('/albums/{id}', [AlbumController::class, 'update'])->name('albums.update');
+    Route::delete('/albums/{id}', [AlbumController::class, 'delete'])->name('albums.delete');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Songs
+|--------------------------------------------------------------------------
+*/
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/songs', [SongController::class, 'list'])->name('songs.list');
+    Route::post('/songs', [SongController::class, 'create'])->name('songs.create');
+    Route::put('/songs/{id}', [SongController::class, 'update'])->name('songs.update');
+    Route::delete('/songs/{id}', [SongController::class, 'delete'])->name('songs.delete');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Reactions
+|--------------------------------------------------------------------------
+*/
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('songs/{songId}/reactions/{reactionType}', [ReactionController::class, 'addOrUpdateReaction']);
+    Route::delete('songs/{songId}/reactions', [ReactionController::class, 'removeReaction']);
+    Route::get('songs/{songId}/reactions', [ReactionController::class, 'getReaction']);
+});
+
+/*
+|--------------------------------------------------------------------------
+| Companies
+|--------------------------------------------------------------------------
+*/
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('companies', CompanyController::class);
+});
+
+/*
+|--------------------------------------------------------------------------
+| Cars
+|--------------------------------------------------------------------------
+*/
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('cars', CarController::class);
+});
+
+/*
+|--------------------------------------------------------------------------
+| Car Rates
+|--------------------------------------------------------------------------
+*/
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('car-rates', CarRateController::class);
+});
+
+/*
+|--------------------------------------------------------------------------
+| Bookings
+|--------------------------------------------------------------------------
+*/
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('bookings', BookingController::class);
 });
