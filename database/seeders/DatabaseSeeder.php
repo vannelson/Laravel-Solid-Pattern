d34dbad7e6eec132282d9bac579a5478388832fc
vannelson <vannelsonsimumbay@gmail.com>
2025-10-21 14:43:51 +0800

    arrange migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
+            UserPasswordResetSeeder::class,
             CompanySeeder::class,
             CarSeeder::class,
             BookingSeeder::class,
